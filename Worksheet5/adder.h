f067050d500d95a687a0519492c9610536bead77
Your Name <eeyna4@nottingham.ac.uk.com>
2026-02-13 14:59:54 +0000

    Add doxygen comments to adder.h

--- Worksheet5/adder.h
+++ Worksheet5/adder.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * @file adder.h
+ * @brief Public API for the add function.
+ */
+
 #ifdef _WIN32
 #ifdef mathsEXPORTS
 #define MATHS_API __declspec(dllexport)
@@ -10,4 +15,10 @@
 #define MATHS_API
 #endif
 
+ /**
+  * @brief Adds two integers.
+  * @param a First integer.
+  * @param b Second integer.
+  * @return The sum a + b.
+  */
 extern "C" MATHS_API int add(int a, int b);
