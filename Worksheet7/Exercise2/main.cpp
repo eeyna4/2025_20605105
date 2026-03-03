c64ef45ec0b3b694be2df69d57f315323cc235cd
Your Name <eeyna4@nottingham.ac.uk.com>
2026-03-03 12:21:11 +0000

    Worksheet7 Exercise2: switch to OpenVR renderer and cylinder source

--- Worksheet7/Exercise2/main.cpp
+++ Worksheet7/Exercise2/main.cpp
@@ -1,52 +1,40 @@
-#include <iostream>
-
 #include <vtkActor.h>
 #include <vtkNew.h>
+#include <vtkOpenVRCamera.h>
+#include <vtkOpenVRRenderWindow.h>
+#include <vtkOpenVRRenderWindowInteractor.h>
+#include <vtkOpenVRRenderer.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
-#include <vtkSphereSource.h>
+#include <vtkCylinderSource.h>
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    (void)argc; (void)argv;
 
-    std::cout << "[VTK] Program start" << std::endl;
-
-    vtkNew<vtkSphereSource> sphere;
-    sphere->SetRadius(0.5);
-    sphere->SetThetaResolution(32);
-    sphere->SetPhiResolution(32);
+    vtkNew<vtkCylinderSource> cyl;
+    cyl->SetRadius(0.2);
+    cyl->SetHeight(1.0);
+    cyl->SetResolution(64);
 
     vtkNew<vtkPolyDataMapper> mapper;
-    mapper->SetInputConnection(sphere->GetOutputPort());
+    mapper->SetInputConnection(cyl->GetOutputPort());
 
     vtkNew<vtkActor> actor;
     actor->SetMapper(mapper);
 
-    vtkNew<vtkRenderer> renderer;
+    vtkNew<vtkOpenVRRenderer> renderer;
     renderer->AddActor(actor);
-    renderer->SetBackground(0.1, 0.2, 0.3);
 
-    vtkNew<vtkRenderWindow> rw;
+    vtkNew<vtkOpenVRRenderWindow> rw;
     rw->AddRenderer(renderer);
-    rw->SetSize(900, 600);
-    rw->SetWindowName("Worksheet7 VTK Sphere");
+    rw->SetWindowName("Worksheet7 Exercise2 - OpenVR Cylinder");
 
-    vtkNew<vtkRenderWindowInteractor> iren;
+    vtkNew<vtkOpenVRRenderWindowInteractor> iren;
     iren->SetRenderWindow(rw);
 
-    std::cout << "[VTK] Before Render()" << std::endl;
     rw->Render();
-
-    std::cout << "[VTK] Before Initialize()" << std::endl;
     iren->Initialize();
-
-    std::cout << "[VTK] Before Start() (window should appear now)" << std::endl;
     iren->Start();
 
-    std::cout << "[VTK] After Start() (printed after window closes)" << std::endl;
     return 0;
 }
