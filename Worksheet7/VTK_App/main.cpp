30f7e7906ec365d057c3c77bb12fc4b30bbf56b2
Your Name <eeyna4@nottingham.ac.uk.com>
2026-03-03 11:39:06 +0000

    Worksheet7: VTK app + ignore build folder

--- Worksheet7/VTK_App/main.cpp
+++ Worksheet7/VTK_App/main.cpp
@@ -1,36 +1,52 @@
-#include <vtkSmartPointer.h>
-#include <vtkSphereSource.h>
-#include <vtkPolyDataMapper.h>
+#include <iostream>
+
 #include <vtkActor.h>
-#include <vtkRenderer.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSphereSource.h>
 
-int main(int, char*[])
+int main(int argc, char* argv[])
 {
-  auto sphere = vtkSmartPointer<vtkSphereSource>::New();
-  sphere->SetRadius(0.5);
-  sphere->SetThetaResolution(32);
-  sphere->SetPhiResolution(32);
+    (void)argc;
+    (void)argv;
 
-  auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapper->SetInputConnection(sphere->GetOutputPort() );
+    std::cout << "[VTK] Program start" << std::endl;
 
-  auto actor = vtkSmartPointer<vtkActor>::New();
-  actor->SetMapper(mapper);
+    vtkNew<vtkSphereSource> sphere;
+    sphere->SetRadius(0.5);
+    sphere->SetThetaResolution(32);
+    sphere->SetPhiResolution(32);
 
-  auto renderer = vtkSmartPointer<vtkRenderer>::New();
-  renderer->AddActor(actor);
-  renderer->SetBackground(0.1, 0.2, 0.3);
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(sphere->GetOutputPort());
 
-  auto rw = vtkSmartPointer<vtkRenderWindow>::New();
-  rw->AddRenderer(renderer);
-  rw->SetSize(900, 600);
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
 
-  auto iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  iren->SetRenderWindow(rw);
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->SetBackground(0.1, 0.2, 0.3);
 
-  rw->Render();
-  iren->Start();
-  return 0;
+    vtkNew<vtkRenderWindow> rw;
+    rw->AddRenderer(renderer);
+    rw->SetSize(900, 600);
+    rw->SetWindowName("Worksheet7 VTK Sphere");
+
+    vtkNew<vtkRenderWindowInteractor> iren;
+    iren->SetRenderWindow(rw);
+
+    std::cout << "[VTK] Before Render()" << std::endl;
+    rw->Render();
+
+    std::cout << "[VTK] Before Initialize()" << std::endl;
+    iren->Initialize();
+
+    std::cout << "[VTK] Before Start() (window should appear now)" << std::endl;
+    iren->Start();
+
+    std::cout << "[VTK] After Start() (printed after window closes)" << std::endl;
+    return 0;
 }
